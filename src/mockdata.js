e0863b98247d49df55de0408a8d7141f51c5dad0
kaushalravin <kaushalravin007@gmail.com>
2025-12-14 11:27:10 +0530

    deleted mock data

--- src/mockdata.js
+++
@@ -1,9 +0,0 @@
-export const mockdata = [
-  {
-    login: "octocat",
-    id: 1,
-    type: "User",
-    avatar_url: "https://avatars.githubusercontent.com/u/583231",
-    html_url: "https://github.com/octocat"
-  }
-];
